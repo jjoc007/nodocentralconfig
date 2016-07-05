6ec7b1c9e5017e2bc3a712517d47d64d7b0917e7
jjoc007 <j.j.o.c007@gmail.com>
2016-07-04 21:11:00 -0500

    se agrega la generacion automatica de tablas desde el app a la base de datos

--- main.go
+++ main.go
@@ -7,10 +7,20 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/lib/pq"
+
+	"fmt"
+
 )
 
 func init() {
 	orm.RegisterDataBase("default", "postgres", "postgres://postgres:123456@127.0.0.1:5432/nodocentral")
+
+	//sincroniza base de datos
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+	    fmt.Println(err)
+	}
+
 }
 
 func main() {
